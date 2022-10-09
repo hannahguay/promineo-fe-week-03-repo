8bc1509b59fdba425ee3d4faecd05b7a9130a487
Hannah Guay <hannah.n.guay@gmail.com>
2022-10-09 09:34:58 -0600

    add html and js files

---
+++ script.js
@@ -0,0 +1,38 @@
+// ARRAY
+
+// let customerNames = [];
+// customerNames.push('Sam Smith');
+// customerNames.push('Tommy Guns');
+// customerNames.push('Jimmy John');
+// customerNames.push('Carrie Cardigan');
+
+// console.log(customerNames[0])
+
+// for (let i = 0; i < customerNames.length; i++) {
+//     console.log(customerNames[i])
+// }
+
+// for (name of customerNames) {
+//     console.log(name);
+// }
+
+// FUNCTION
+
+// function myFunction() {
+//     for (let i = 0; i < 100; i++) {
+//     console.log(i);
+//     }
+// }
+
+// myFunction()
+
+// function createFullName(firstName, lastName) {
+//     return firstName + ' ' + lastName;
+// }
+// //              ^parameters       ^parameters
+
+// console.log(createFullName('Tom', 'Sawyer'));
+
+// //              ^arguments ^arguments
+
+// let fullName = createFullName('Tom', 'Sawyer');
